be9480a987a0e85734328313b3e40b38c5f62ca3
Kristina <kristina.m.jefferson@gmail.com>
2020-08-31 16:42:19 -0500

    implement customText function

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const textInput = document.querySelector('.custom-text');
-const cardText = document.querySelector('.card');
 
 function customText(){
-    
-    cardText.innerHTML = textInput.value;
+    const cardText = document.querySelectorAll('.font-card');
+    cardText.forEach( cardNode => cardNode.innerHTML = textInput.value);  
 }
+
+function createCards(){
+    const tempFontArray = ["font1", "font2", "font3", "font4", "font5", "font6"];
+    const cards = tempFontArray.map((card) => {
+        return `
+        <span class="column">
+            <span class="font-card">
+                This is some sample text.
+            </span>
+            
+        </span>
+        `;
+    });
+    
+    loadCards.innerHTML = cards;
+}
+
+const loadCards = document.querySelector('.font-card-container');
+window.onload = createCards;
+
 textInput.addEventListener('change', customText);
 textInput.addEventListener('keyup', customText);
