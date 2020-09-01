9ef7e183dceb4dfd5d2537c6c1a6bdc6b49d029d
Kristina <kristina.m.jefferson@gmail.com>
2020-09-01 08:54:01 -0500

    add condition to revert to default text

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const textInput = document.querySelector('.custom-text');
+const defaultText = "This sentence is the default sentence."
 
 function customText(){
     const cardText = document.querySelectorAll('.font-text');
-    cardText.forEach( cardNode => cardNode.innerHTML = textInput.value);  
+    cardText.forEach( cardNode => {
+        if(textInput.value == ""){cardNode.innerHTML = defaultText;}
+        else{cardNode.innerHTML = textInput.value;}
+    });
+    
 }
 
 function createCards(){
@@ -12,12 +17,12 @@ function createCards(){
         <span class="column">
             <span class="font-card">
                 <h2>${card}</h2>
-                <p style="font-family:${card}" class="font-text">This is some sample text.</p>
+                <p style="font-family:${card}" class="font-text">${defaultText}</p>
             </span>
             
         </span>
-        `;
-    });
+        `
+    }).join('');
     
     loadCards.innerHTML = cards;
 }
