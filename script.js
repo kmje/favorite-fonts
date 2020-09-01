906614fc78e2061327a36885b89ab6c6a14b2cab
Kristina <kristina.m.jefferson@gmail.com>
2020-09-01 01:14:35 -0500

    add fonts

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const textInput = document.querySelector('.custom-text');
 
 function customText(){
-    const cardText = document.querySelectorAll('.font-card');
+    const cardText = document.querySelectorAll('.font-text');
     cardText.forEach( cardNode => cardNode.innerHTML = textInput.value);  
 }
 
 function createCards(){
-    const tempFontArray = ["font1", "font2", "font3", "font4", "font5", "font6"];
-    const cards = tempFontArray.map((card) => {
+    const fontArray = ["Lora", "Tangerine", "Inconsolata", "Roboto", "Raleway", "Krona One", "Dosis", "Lobster", "Abel", "Caveat"];
+    const cards = fontArray.map((card) => {
         return `
         <span class="column">
             <span class="font-card">
-                This is some sample text.
+                <h2>${card}</h2>
+                <p style="font-family:${card}" class="font-text">This is some sample text.</p>
             </span>
             
         </span>
